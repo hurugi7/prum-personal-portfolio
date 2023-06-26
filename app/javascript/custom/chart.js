faa78b5a0603ff4c2aea6c1b20936ff1804a58cf
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-06-26 12:34:15 +0900

    create home page

---
+++ app/javascript/custom/chart.js
@@ -0,0 +1,28 @@
+const ctx = document.getElementById('myChart');
+
+const chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['January', 'February', 'March'],
+    datasets: [{
+      label: 'Dataset 1',
+      data: [-17, -10, -60],
+      borderWidth: 1.5,
+      backgroundColor: 'rgba(247, 141, 184, 0.7)',
+      borderColor: 'rgba(247, 141, 184)'
+    },{
+      label: 'Dataset 2',
+      data: [5, 10, -9],
+      borderWidth: 1.5,
+      backgroundColor: 'rgba(255, 180, 110, 0.7)',
+      borderColor: 'rgba(255, 180, 110)'
+    },{
+      label: 'Dataset 3',
+      data: [90, -18, 18],
+      borderWidth: 1.5,
+      backgroundColor: 'rgba(250, 212, 125, 0.7)',
+      borderColor: 'rgba(250, 212, 125)'
+    }
+  ]
+  }
+});
