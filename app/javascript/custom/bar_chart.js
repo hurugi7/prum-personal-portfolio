9a5f17b8122ca1bef596c90298bde3fa587338d6
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-06-27 19:32:45 +0900

    display bar_chart

---
+++ app/javascript/custom/bar_chart.js
@@ -0,0 +1,21 @@
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['January', 'February', 'March'],
+    datasets: [{
+      label: 'Dataset 1',
+      data: [0, 0, 0],
+      borderWidth: 1
+    },{
+      label: 'Dataset 2',
+      data: [0, 0, 0],
+      borderWidth: 1
+    },{
+      label: 'Dataset 3',
+      data: [0, 0, 0],
+      borderWidth: 1
+    }]
+  }
+});
